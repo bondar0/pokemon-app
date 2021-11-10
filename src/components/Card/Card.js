1c7a727e8b3f58ee32ada8f29bb220830e9394d0
Kamil Bondaruk <kamil.bondaruk@gmail.com>
2021-11-10 16:59:50 +0100

    Update styles for list

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import PokemonImg from 'assets/img/pokemon.png';
+import styles from './Card.module.css';
 
 const Card = ({ name, image }) => {
   return (
     <div className="col-sm-6 col-xl-3">
       <div className="card mb-3">
-        <img className="card-img-top" src={image} alt="Card image Pokemon" />
+        <div className={styles.imgWrapper}>
+          <img className="card-img-top" src={image} alt="Card image Pokemon" />
+        </div>
+
         <div className="card-body text-center">
           <h5 className="card-title font-weight-bold">{name}</h5>
           <Link to={`/pokemons/${name}`} className="btn btn-info">
